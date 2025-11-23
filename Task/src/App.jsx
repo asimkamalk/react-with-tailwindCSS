7813be2f7c5299171140170fcbe1a84b0227f921
Asim Kamal <asimkamalk@gmail.com>
2025-11-24 03:04:50 +0500

    practicing passing props

--- Task/src/App.jsx
+++ Task/src/App.jsx
@@ -7,14 +7,20 @@ const { name, occu } = obj;
 console.log(name);
 console.log(occu);
 
+const PracticeProps = (porps) => {
+  const { exampleProps } = porps;
+  return <div>just practicing props {exampleProps}</div>;
+};
+
 const Message = (props) => {
-  const { text, bgColor } = props;
+  const { text, bgColor, fontColor } = props;
   console.log(text, bgColor);
 
   return (
     <div
       style={{
         background: bgColor,
+        color: fontColor,
       }}
     >
       {text}
@@ -34,6 +40,25 @@ const Description = () => {
   return <div>This is my second react compoenent</div>;
 };
 
+// Box component
+
+const Box = (props) => {
+  const { label, bgColor, textColor, large } = props;
+  console.log(large);
+
+  return (
+    <div
+      style={{
+        backgroundColor: bgColor,
+        color: textColor,
+        fontSize: "40px",
+      }}
+    >
+      {label}
+    </div>
+  );
+};
+
 function App() {
   return (
     <>
@@ -41,7 +66,24 @@ function App() {
       <Title />
       <Title />
       <Description />
-      <Message text="this is my first prop" bgColor="red" />
+      <Message text="this is my first prop" bgColor="red" fontColor="white" />
+      <Message text="hi" bgColor="green" fontColor="white" />
+      <Message text="bye" bgColor="blue" fontColor="white" />
+      <br />
+      <br />
+      <PracticeProps exampleProps="This is my test props it comes from App" />
+
+      <br />
+      <br />
+      <br />
+
+      <Box
+        label="my name is Asim"
+        bgColor="CornflowerBlue"
+        textColor="White"
+        large={true}
+      />
+      {/* <Box label="another one" bgColor="lavender" textColor="purple" /> */}
     </>
   );
 }
