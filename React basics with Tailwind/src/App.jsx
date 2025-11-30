24cc13a19fc1df83c48d243bde8aa306f7874813
Asim Kamal <asimkamalk@gmail.com>
2025-11-30 23:09:00 +0500

    feat: Refactor App into Title and Description components

--- React basics with Tailwind/src/App.jsx
+++ React basics with Tailwind/src/App.jsx
@@ -1,5 +1,21 @@
-function App() {
-  return <h1>Hello</h1>;
-}
+const Title = () => {
+  return <div>Hello World</div>;
+};
+
+const Description = () => {
+  return <div>Another component</div>;
+};
+
+const App = () => {
+  return (
+    <div>
+      Hello
+      <Title />
+      <Title />
+      <Title />
+      <Description />
+    </div>
+  );
+};
 
 export default App;
