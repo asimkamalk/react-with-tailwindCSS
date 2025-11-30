5badea416b2bdaf52f03de796791983dd24303fc
Asim Kamal <asimkamalk@gmail.com>
2025-12-01 03:00:02 +0500

    feat: Introduce TailwindBasics component to demonstrate Tailwind CSS

--- React basics with Tailwind/src/App.jsx
+++ React basics with Tailwind/src/App.jsx
@@ -3,49 +3,54 @@ import Description from "./Description.jsx";
 import Message from "./Message.jsx";
 import Box from "./Box.jsx";
 import MyCompoonent from "./MyComponent.jsx";
+import TailwindBasics from "./TailwindBasics.jsx";
 
 const App = () => {
   return (
-    <div>
-      Hello
-      <Title />
-      <Title />
-      <Title />
-      <Description />
-      <br />
-      {/* Props */}
-      <Message text="This is my first props" bgColor="red" />
-      <Message text="Hi!" bgColor="green" />
-      <Message text="Bye!" bgColor="yellow" />
-      <br />
-      {/* Box */}
-      <Box
-        label="My name is Asim Kamal"
-        bgColor="red"
-        fontColor="white"
-        large={true}
-      />
-      <Box
-        label="another box"
-        bgColor="purple"
-        fontColor="white"
-        large={false}
-      />
-      <Box
-        label="Green Box"
-        bgColor="green"
-        fontColor="purple"
-        // large will be undefined
-      />
-      <br />
-      {/* My Component */}
-      <MyCompoonent bgColor="yellow" height={90}>
-        Hello
-      </MyCompoonent>
-      <MyCompoonent bgColor="lavender" height={40}>
-        Bye
-      </MyCompoonent>
-    </div>
+    // TailwindCSS started here
+
+    <TailwindBasics text="One" styles="bg-red-700 text-rose-300" />
+
+    // <div>
+    //   Hello
+    //   <Title />
+    //   <Title />
+    //   <Title />
+    //   <Description />
+    //   <br />
+    //   {/* Props */}
+    //   <Message text="This is my first props" bgColor="red" />
+    //   <Message text="Hi!" bgColor="green" />
+    //   <Message text="Bye!" bgColor="yellow" />
+    //   <br />
+    //   {/* Box */}
+    //   <Box
+    //     label="My name is Asim Kamal"
+    //     bgColor="red"
+    //     fontColor="white"
+    //     large={true}
+    //   />
+    //   <Box
+    //     label="another box"
+    //     bgColor="purple"
+    //     fontColor="white"
+    //     large={false}
+    //   />
+    //   <Box
+    //     label="Green Box"
+    //     bgColor="green"
+    //     fontColor="purple"
+    //     // large will be undefined
+    //   />
+    //   <br />
+    //   {/* My Component */}
+    //   <MyCompoonent bgColor="yellow" height={90}>
+    //     Hello
+    //   </MyCompoonent>
+    //   <MyCompoonent bgColor="lavender" height={40}>
+    //     Bye
+    //   </MyCompoonent>
+    // </div>
   );
 };
 
---
+++ React basics with Tailwind/src/TailwindBasics.jsx
@@ -0,0 +1,15 @@
+const TailwindBasics = () => {
+  return (
+    <div>
+      <div className="bg-red-700 text-rose-300">One</div>
+      <div className="bg-slate-300 border border-slate-600">Two</div>
+      <div className="text-3xl text-blue-300 bg-emerald-900">Three</div>
+      <div className="text-xl text-purple-500">
+        <div className="border border-sky">Four</div>
+        <div className=" bg-violet-200 border-l-8 border-purple-800 ">Five</div>
+      </div>
+    </div>
+  );
+};
+
+export default TailwindBasics;
