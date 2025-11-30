1932a855dabdede13eae3ab553f94315213b16e9
Asim Kamal <asimkamalk@gmail.com>
2025-11-30 23:40:28 +0500

    feat: Add Message and Box components to demonstrate props

--- React basics with Tailwind/src/App.jsx
+++ React basics with Tailwind/src/App.jsx
@@ -6,6 +6,36 @@ const Description = () => {
   return <div>Another component</div>;
 };
 
+const Message = (props) => {
+  const { text, bgColor } = props;
+  return (
+    <div
+      style={{
+        backgroundColor: bgColor,
+      }}
+    >
+      {text}
+    </div>
+  );
+};
+
+const Box = (props) => {
+  const { label, bgColor, fontColor, large } = props;
+  // console.log(large);
+
+  return (
+    <div
+      style={{
+        backgroundColor: bgColor,
+        color: fontColor,
+        fontSize: large ? "40px" : "12px",
+      }}
+    >
+      {label}
+    </div>
+  );
+};
+
 const App = () => {
   return (
     <div>
@@ -14,6 +44,31 @@ const App = () => {
       <Title />
       <Title />
       <Description />
+      <br />
+      {/* Props */}
+      <Message text="This is my first props" bgColor="red" />
+      <Message text="Hi!" bgColor="green" />
+      <Message text="Bye!" bgColor="yellow" />
+      <br />
+      {/* Box */}
+      <Box
+        label="My name is Asim Kamal"
+        bgColor="red"
+        fontColor="white"
+        large={true}
+      />
+      <Box
+        label="another box"
+        bgColor="purple"
+        fontColor="white"
+        large={false}
+      />
+      <Box
+        label="Green Box"
+        bgColor="green"
+        fontColor="purple"
+        // large will be undefined
+      />
     </div>
   );
 };
